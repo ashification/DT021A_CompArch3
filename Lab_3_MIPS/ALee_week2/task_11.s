024417d93ed833b08d2c142357c506eb863c1492
Aisling Lee <aislinglee@gmail.com>
2022-11-18 16:54:51 +0000

    Uploading Lab 3 MIPS32 Machine Language Instructions Study

---
+++ Lab_3_MIPS/ALee_week2/task_11.s
@@ -0,0 +1,14 @@
+	 .set noreorder  			# Avoid reordering instructions
+ 	 .text 						# Start generating instructions
+	 .globl	start 				# The label should be globally known
+	 .ent start 				# The label marks an entry point
+	 
+start:	 addi $8, $0, 0x1 	# Load the value 1
+ 		 addi $9, $0, 0x2 	# Load the value 1
+		 add $10, $8, $9 	# Add the values
+		 addi $8, $0, 0xFF 	# into reg X , from addr X , load value
+		
+		 b start 			# branch back to start (loop)
+		 nop				# No Operation
+		 .end start			# Marks the end of the program
+		 
